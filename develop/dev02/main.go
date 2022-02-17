7f6a47758687ea858bb93d01357f1e9476d24401
KuschenkoEgor <kushchenkoYA@gmail.com>
2022-02-17 15:20:36 +0300

    Add dev02

---
+++ develop/dev02/main.go
@@ -0,0 +1,77 @@
+package main
+
+//Создать Go-функцию, осуществляющую примитивную
+//распаковку строки, содержащую повторяющиеся символы/руны,
+//например:
+//"a4bc2d5e" => "aaaabccddddde"
+//"abcd" => "abcd"
+//"45" => "" (некорректная строка)
+//"" => ""
+
+//Реализовать поддержку escape-последовательностей.
+//Например:
+//qwe\4\5 => qwe45 (*)
+//qwe\45 => qwe44444 (*)
+//qwe\\5 => qwe\\\\\ (*)
+
+//В случае если была передана некорректная строка,
+//функция должна возвращать ошибку. Написать unit-тесты.
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+func ConvertString(rs, answer []rune, k, ln int) []rune {
+	for i := 0; i < ln; i++ {
+
+		if unicode.IsDigit(rs[0]) {
+			break
+		}
+
+		if unicode.IsLetter(rs[i]) {
+			answer = append(answer, rs[i])
+
+		} else if unicode.IsDigit(rs[i]) && i != 0 && unicode.IsLetter(rs[i-1]) {
+			x, _ := strconv.Atoi(string(rs[i]))
+			repeat := strings.Repeat(string(rs[i-1]), x-1)
+			rp := []rune(repeat)
+			answer = append(answer, rp...)
+
+		} else if i != 0 && strings.Contains(string(rs[i-1]), `\`) && strings.Contains(string(rs[i-2]), `\`) != true {
+			answer = append(answer, rs[i])
+
+		} else if unicode.IsDigit(rs[i]) && i != 0 && strings.Contains(string(rs[i-2]), `\`) {
+			x, _ := strconv.Atoi(string(rs[i]))
+			repeat := strings.Repeat(string(rs[i-1]), x-1)
+			rp := []rune(repeat)
+			answer = append(answer, rp...)
+		}
+
+	}
+	for _ = range answer {
+		k++
+	}
+	if k == 0 {
+		err := fmt.Errorf("Ошибка! Некорректная строка")
+		fmt.Println(err.Error())
+	}
+
+	return answer
+}
+
+func main() {
+	var k int
+	var Str string
+	fmt.Println("Введите строку:")
+	fmt.Scan(&Str)
+	rs := []rune(Str)
+	answer := make([]rune, 0)
+	ln := utf8.RuneCountInString(Str)
+
+	answer = ConvertString(rs, answer, k, ln)
+
+	fmt.Println("Отформатированная строка:", string(answer))
+}
